c0afc9d91ab422b36ddd3eb1ed50fb60d2bcce64
Abdallah-Allaham <allham.abod@gmail.com>
2025-07-23 14:21:55 +0300

    Update PorcupainService.java

--- android/app/src/main/java/com/example/nabd/PorcupainService.java
+++ android/app/src/main/java/com/example/nabd/PorcupainService.java
@@ -38,7 +38,7 @@ public class PorcupainService extends Service {
     private int bufferIndex = 0;
     private boolean isRecording = false;
 
-    String apiKey = "q/DiiXIUQ5xUOFNI06Y3FuFA64EDBWo3MrZJ5tZVDI1j8K8Zp+QugA==";
+    String apiKey = "";
 
     @Override
     public void onCreate() {
